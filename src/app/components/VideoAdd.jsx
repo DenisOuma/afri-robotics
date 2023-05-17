0554133204cecde29f76e42cd1ccdaadb771bfb3
Denis Ouma <dennisouma38@gmail.com>
2023-05-17 19:01:01 +0300

    video update

--- src/app/components/VideoAdd.jsx
+++ src/app/components/VideoAdd.jsx
@@ -15,7 +15,7 @@ export const VideoComponent = () => {
 		<div className="video-container">
 			<div className="top-color"></div>
 			<video autoPlay loop muted className="video-element w-full">
-				<source src="/videos/advert.mp4" type="video/mp4" />
+				<source src="/videos/advert_ZjR1p0cU.mp4" type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
 		</div>
