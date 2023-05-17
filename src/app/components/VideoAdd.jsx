6555430e2f22636fcfca7fcd569aca388cfd2b8e
Denis Ouma <dennisouma38@gmail.com>
2023-05-17 19:27:18 +0300

    videos advert

--- src/app/components/VideoAdd.jsx
+++ src/app/components/VideoAdd.jsx
@@ -1,15 +1,5 @@
-// import React from "react";
-
-// export const VideoComponent = () => {
-// 	return (
-// 		<video controls>
-// 			<source src="/videos/advert.mp4" type="video/mp4" />
-// 			Your browser does not support the video tag.
-// 		</video>
-// 	);
-// };
-
 import React from "react";
+import ReactPlayer from "react-player";
 export const VideoComponent = () => {
 	return (
 		<div className="video-container">
