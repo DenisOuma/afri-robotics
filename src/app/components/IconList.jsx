babc8aa27a5b34978e5e3daf1fece48aa917bb95
Denis Ouma <dennisouma38@gmail.com>
2023-05-17 15:52:57 +0300

    Setting up the application STEM section completed

---
+++ src/app/components/IconList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const IconList = () => {
+	return (
+		<div className="h-5 w-5 mr-5 rounded-full border-4 border-secondary-color"></div>
+	);
+};
--- src/app/components/afibotservices/StemEducation.jsx
+++ src/app/components/afibotservices/StemEducation.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 // import Icon from "@iconify/react";
 import robotImage from "../../../../assets/images/robotcar-removebg-preview.png";
+import { IconList } from "../IconList";
 export const StemEducation = () => {
 	return (
 		<div className="flex items-center justify-between   w-full">
@@ -13,26 +14,29 @@ export const StemEducation = () => {
 				/>
 			</div>
 			<div className="flex items-center justify-between m-auto full">
-				<div className="mt-[1rem] w-3/4">
-					<div className="flex items-center">
+				<div className="mt-[1rem] w-6/5">
+					<div className="flex items-center ">
 						<div className="h-10 w-[6px] bg-secondary-color mr-[4px]"></div>
 						<h1 className=" text-secondary-color font-[600] tracking-wider">
 							STEM, Robotics, and AI Education
 						</h1>
 					</div>
-					<h1 className="capitalize text-[1.5rem] font-bold leading-snug">
+					<h1 className="capitalize text-[1.5rem] font-bold pt-[1rem] leading-snug">
 						Promoting Innovation, Skill Development, and Collaborative Learning
 					</h1>
 
-					<div className="mt-5 w-3/4">
+					<div className="mt-5 w-3/4 list-elements">
 						<ul>
 							<li>
+								<IconList />
 								<p>Robotics and AI Labs for K-12 and Higher Education</p>
 							</li>
 							<li>
+								<IconList />
 								<p>Apprenticeship Programs for Hands-On Experience</p>
 							</li>
 							<li>
+								<IconList />
 								<p>
 									Coding and Robotics Festivals for Inspiring Creativity and
 									Exploration
