c111a48ef83007c4765652155a7a202c9de0b383
Denis Ouma <dennisouma38@gmail.com>
2023-05-17 16:53:45 +0300

    Updating the r&d section and comp;eted design

--- src/app/components/afibotservices/ProductManagementRD.jsx
+++ src/app/components/afibotservices/ProductManagementRD.jsx
@@ -11,29 +11,38 @@ export const ProductManagementRD = () => {
 					<div className="flex items-center ">
 						<div className="h-10 w-[6px] bg-secondary-color mr-[4px]"></div>
 						<h1 className=" text-secondary-color font-[600] tracking-wider">
-							STEM, Robotics, and AI Education
+							Advanced Product R&D and Manufacturing Solutions
 						</h1>
 					</div>
 					<h1 className="capitalize text-[1.5rem] font-bold pt-[1rem] leading-snug">
-						Promoting Innovation, Skill Development, and Collaborative Learning
+						Tailored Services for Custom Electronics Development
 					</h1>
 
 					<div className="mt-5 w-3/4 list-elements">
 						<ul>
 							<li>
 								<IconList />
-								<p>Robotics and AI Labs for K-12 and Higher Education</p>
+								<p>Custom Electronics Development</p>
 							</li>
 							<li>
 								<IconList />
-								<p>Apprenticeship Programs for Hands-On Experience</p>
+								<p>Firmware Development</p>
 							</li>
 							<li>
 								<IconList />
-								<p>
-									Coding and Robotics Festivals for Inspiring Creativity and
-									Exploration
-								</p>
+								<p>Computer Simulation</p>
+							</li>
+							<li>
+								<IconList />
+								<p>Enclosure Design</p>
+							</li>
+							<li>
+								<IconList />
+								<p>Rapid Prototyping</p>
+							</li>
+							<li>
+								<IconList />
+								<p>Manufacturing Support & Sourcing</p>
 							</li>
 						</ul>
 					</div>
